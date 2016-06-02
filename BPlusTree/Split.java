082a8187578d111e82c293aee469d33f7585e4a6
kozer1223 <kozerskilo@interia.pl>
2016-06-02 15:30:06 +0000

    Add files via upload

---
+++ BPlusTree/Split.java
@@ -0,0 +1,14 @@
+
+public class Split<K extends Comparable<K>, V> {
+	
+	public final K key;
+	public final Node<K,V> left;
+	public final Node<K,V> right;
+
+	public Split(K key, Node<K,V> left, Node<K,V> right) {
+		this.key = key;
+		this.left = left;
+		this.right = right;
+	}
+
+}
