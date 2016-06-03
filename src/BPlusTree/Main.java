9cfb6d6481b3591ee6972ffc23c4576bbd864709
kozer1223 <kozerskilo@interia.pl>
2016-06-03 18:33:47 +0200

    Update Main.java

--- src/BPlusTree/Main.java
+++ src/BPlusTree/Main.java
@@ -4,33 +4,6 @@ public class Main {
 
 	public static void main(String[] args){
 		BPlusTree<Integer, Integer> tree = new BPlusTree<Integer, Integer>(2);
-		/*tree.insert(5, 5);
-		tree.insert(1, 1);
-		tree.insert(2, 2);
-		System.out.println("DONE 2");
-		tree.dump();
-		tree.insert(3, 3);
-		System.out.println("DONE 3");
-		tree.dump();
-		tree.insert(4, 4);
-		System.out.println("DONE 4");
-		tree.dump();
-		tree.insert(6, 6);
-		System.out.println("DONE 6");
-		tree.dump();
-		tree.insert(7, 7);
-		System.out.println("DONE 7");
-		tree.dump();
-		tree.insert(8, 8);
-		System.out.println("DONE 8");
-		tree.dump();
-		tree.insert(9, 9);
-		System.out.println("DONE 9");
-		tree.dump();
-		tree.insert(12, 12);
-		System.out.println("DONE 12");
-		tree.dump();*/
-		
 		int[] array = {1,4,16,25,9,20,13,15,10,11,12,21,22,23};
 		for(int i=0; i<array.length; i++){
 			tree.insert(array[i], array[i]);
@@ -49,12 +22,7 @@ public class Main {
 				tree2.insert(i, i);
 			}
 		}
-		/*for(int i=0; i<=2100; i++){
-			if (i%2 == 1){
-				tree2.insert(i, i);
-			}
-		}*/
-		//tree2.dump();
+
 		for(int i=0; i<=200; i++){
 			System.out.println(i + ": " + tree2.retrieve(i));
 		}
